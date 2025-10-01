6f56348277d2a9887a8cfa36ba3e94a6e0299712
moosaad <muhammedsaad.x1@gmail.com>
2025-10-01 20:25:35 +0300

    finished favourite tab ui

--- lib/features/main_layout/favourite_tab/favourite_tab.dart
+++ lib/features/main_layout/favourite_tab/favourite_tab.dart
@@ -1,12 +1,45 @@
+import 'package:evently_sun_online/core/resources/colors_manager.dart';
+import 'package:evently_sun_online/core/widgets/event_item.dart' show EventItem;
+import 'package:evently_sun_online/models/category_model.dart';
+import 'package:evently_sun_online/models/event_model.dart' show EventModel;
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FavouriteTab extends StatelessWidget {
   const FavouriteTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.black,
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+
+              decoration: InputDecoration(
+
+                prefixIcon: Icon(Icons.search),
+                prefixIconColor: ColorsManager.blue,
+                hintText: "Search for Event",
+                hintStyle: GoogleFonts.inter(fontSize: 14.sp, fontWeight: FontWeight.bold, color: ColorsManager.blue)
+              ),
+            ),
+          ),
+          SizedBox(height: 16.h,),
+          Expanded(child: ListView.builder(itemBuilder: (context, index)=>  EventItem(
+            event: EventModel(
+              category: CategoryModel.categories[2],
+              title: "Meeting for Updating The Development Method ",
+              description: "Meeting for Updating The Development Method ",
+              dateTime: DateTime.now(),
+              timeOfDay: TimeOfDay.now(),
+            ),
+          ), itemCount: 20,))
+
+        ],
+      ),
     );
   }
 }
